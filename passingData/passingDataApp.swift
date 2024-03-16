d0d5e3f39e0b6a7e9615eb156e4f631eb87f552a
VipulNita <vipulknita18@gmail.com>
2024-03-17 00:43:16 +0530

    Initial commit

---
+++ passingData/passingDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  passingDataApp.swift
+//  passingData
+//
+//  Created by vipul kumar on 17/03/24.
+//
+
+import SwiftUI
+
+@main
+struct passingDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
